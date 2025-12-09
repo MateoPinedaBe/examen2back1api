e6401f448ca7eae6e15db18cca4ef0336a1f06dc
Mateo Andres Pineda Beltran <mateo.pineda@globant.com>
2025-12-09 09:50:21 -0500

    Feat: Packages.

---
+++ src/main/java/com/example/examen2Back1/models/Order.java
@@ -0,0 +1,10 @@
+package com.example.examen2Back1.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tbl_orders")
+public class Order {
+    
+}
